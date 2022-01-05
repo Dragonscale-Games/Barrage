2a930bfa8b0c8eaf1114b202a30c02725b211ca1
David-Wong-Cascante <david.wongcascante@gmail.com>
2022-01-04 19:14:21 -0800

    Creating the initial tests for graphics.

---
+++ test/graphics.cpp
@@ -0,0 +1,21 @@
+/* ======================================================================== */
+/*!
+ * \file            graphics.cpp
+ * \author          David Wong Cascante
+ * \par             dragonscale.games.llc@gmail.com
+
+ * \brief
+   The test bed used for testing the graphics code used in the engine.
+ */
+/* ======================================================================== */
+
+/****************************************************************************/
+/*!
+  \brief
+    The entry point for testing graphics code.
+*/
+/****************************************************************************/
+int main(void)
+{
+  return 0;
+}
