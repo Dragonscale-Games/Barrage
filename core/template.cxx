2bab26375b2741d61a53f08151d6fd33d0b5f72e
David-Wong-Cascante <david.wongcascante@gmail.com>
2022-01-09 23:18:53 -0800

    Got every library we have to be added correctly except for RTTR

---
+++ core/template.cxx
@@ -0,0 +1,23 @@
+/* ======================================================================== */
+/*!
+ * \file            template.cxx
+ * \author          David Wong Cascante
+ * \par             dragonscale.games.llc@gmail.com
+
+ * \brief
+   Tests the compilation for all third party libraries.
+ */
+/* ======================================================================== */
+
+/* ======================================================================== */
+/* Includes */
+/* ======================================================================== */
+#include <GLFW/glfw3.h>
+#include <stb_image/stb_image.h>
+#include <glm/glm.hpp>
+#include <rapidjson/rapidjson.h>
+
+int main(void)
+{
+  return 0;
+}
