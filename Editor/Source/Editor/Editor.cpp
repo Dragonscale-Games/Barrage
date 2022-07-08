9cf8bb12d562b748e3551a1e567120c4411841d9
David Wong Cascante <david.wongcascante@gmail.com>
2022-07-07 22:39:34 -0700

    Attempted to get the editor working again though there are issues with engine initialization.

--- Editor/Source/Editor/Editor.cpp
+++ Editor/Source/Editor/Editor.cpp
@@ -53,18 +53,37 @@ namespace Barrage
 
   void Editor::Initialize()
   {
-    Barrage::Engine::Instance = &engine_;
-
     engine_.Initialize();
 
     engine_.GSM().SetGameState(Demo::GS_DemoGame());
 
-    gui_.Initialize(TestRenderer::Instance().GetWindowHandle());
+    Barrage::WindowManager& windowing = engine_.Windowing();
+    Barrage::GfxRegistry2D& registry = engine_.Registry();
+    Barrage::GfxDraw2D& drawing = engine_.Drawing();
+
+    // Register the assets necessary.
+    const char* instancedShaderPaths[] = {
+      "Assets/Shaders/Default/Instanced.vs",
+      "Assets/Shaders/Default/Instanced.fs",
+    };
+    registry.RegisterShader(instancedShaderPaths, "Instanced");
+    registry.RegisterTexture("Assets/Textures/TestBullet.png", "TestBullet");
+    registry.RegisterTexture("Assets/Textures/TestShip.png", "TestShip");
+    // Set any default resources on the draw system.
+    drawing.ApplyShader("Instanced");
+    // Set the viewport of our game.
+    const Barrage::WindowManager::WindowData& settings = windowing.GetSettings();
+    drawing.SetViewportSpace(glm::ivec2(settings.width_, settings.height_));
+
+    //gui_.Initialize(TestRenderer::Instance().GetWindowHandle());
+    gui_.Initialize(windowing.GetInternalHandle());
   }
 
   void Editor::Update()
   { 
     auto t1 = std::chrono::high_resolution_clock::now();
+    Barrage::WindowManager& windowing = engine_.Windowing();
+    Barrage::GfxDraw2D& drawing = engine_.Drawing();
     
     engine_.Input().Update();
 
@@ -74,7 +93,8 @@ namespace Barrage
 
     gui_.EndWidgets();
 
-    TestRenderer::Instance().StartFrame();
+    //TestRenderer::Instance().StartFrame();
+    drawing.StartFrame();
 
     if (!statePaused_)
     {
@@ -92,9 +112,11 @@ namespace Barrage
 
     auto t2 = std::chrono::high_resolution_clock::now();
 
-    TestRenderer::Instance().EndFrame();
+    //TestRenderer::Instance().EndFrame();
+    drawing.EndFrame();
 
-    if (TestRenderer::Instance().WindowClosed())
+    //if (TestRenderer::Instance().WindowClosed())
+    if(!windowing.IsOpen())
       isRunning_ = false;
 
     auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(t2 - t1);
